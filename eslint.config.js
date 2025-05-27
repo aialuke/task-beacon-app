bc5c4f923feeb0332f0764613c0efdb6d2dc56f8
aialuke <79836828+aialuke@users.noreply.github.com>
2025-05-28 02:56:20 +1000

    committing updates

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,6 @@
 import js from "@eslint/js";
 import globals from "globals";
+import reactPlugin from "eslint-plugin-react"; // Import the plugin
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
@@ -7,23 +8,66 @@ import tseslint from "typescript-eslint";
 export default tseslint.config(
   { ignores: ["dist"] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended
+    ],
     files: ["**/*.{ts,tsx}"],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
     plugins: {
+      react: reactPlugin, // Use the imported object
       "react-hooks": reactHooks,
       "react-refresh": reactRefresh,
     },
     rules: {
+      "react/display-name": "warn",
+      "react/jsx-key": "warn",
+      "react/jsx-no-comment-textnodes": "warn",
+      "react/jsx-no-duplicate-props": "warn",
+      "react/jsx-no-target-blank": "warn",
+      "react/jsx-no-undef": "error",
+      "react/jsx-uses-react": "off",
+      "react/jsx-uses-vars": "error",
+      "react/no-children-prop": "warn",
+      "react/no-danger-with-children": "warn",
+      "react/no-deprecated": "warn",
+      "react/no-direct-mutation-state": "error",
+      "react/no-find-dom-node": "warn",
+      "react/no-is-mounted": "warn",
+      "react/no-render-return-value": "error",
+      "react/no-string-refs": "warn",
+      "react/no-unescaped-entities": "warn",
+      "react/no-unknown-property": "warn",
+      "react/no-unsafe": "warn",
+      "react/prop-types": "off",
+      "react/react-in-jsx-scope": "off",
+      "react/require-render-return": "error",
       ...reactHooks.configs.recommended.rules,
       "react-refresh/only-export-components": [
         "warn",
         { allowConstantExport: true },
       ],
-      "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        {
+          vars: "all",
+          args: "after-used",
+          ignoreRestSiblings: true,
+          varsIgnorePattern: "^_",
+          argsIgnorePattern: "^_",
+        },
+      ],
+      "react/no-unused-prop-types": "warn",
+      "react/no-unused-state": "warn",
+      "no-unused-expressions": "warn",
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
     },
   }
 );
