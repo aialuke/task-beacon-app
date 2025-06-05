7d436a84bfd60daea20797b1a84c6e8f9102adca
aialuke <79836828+aialuke@users.noreply.github.com>
2025-06-05 13:15:18 +1000

    Style: Remove height class from UrlInputModal to enhance input field aesthetics

--- src/components/form/UrlInputModal.tsx
+++ src/components/form/UrlInputModal.tsx
@@ -70,7 +70,6 @@ export function UrlInputModal({
               label="URL"
               icon={<Link className="h-4 w-4" />}
               autoFocus
-              className="h-14"
             />
             {error && (
               <p
